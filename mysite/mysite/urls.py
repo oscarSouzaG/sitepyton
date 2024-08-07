4a647a7bc624ad25d03a407358ec1bc2c2a1b65a
oscar souza <osko300@gmail.com>
2024-08-07 10:09:19 -0400

    adicionado pagina de autenticação

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -4,4 +4,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('staticpages.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
+
